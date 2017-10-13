0f1bb21cf0d6ea91ae53b9e7d6ed94c1e72e324a
shivprogrammer <shiv.unadkat@gmail.com>
2017-10-12 17:09:11 -0700

    finished out presplash

--- app/components/PreSplash/PreSplash.js
+++ app/components/PreSplash/PreSplash.js
@@ -3,14 +3,53 @@ import { View, StyleSheet, Image, Animated } from 'react-native'
 
 export default class PreSplash extends Component {
   static PropTypes = {}
-  state = {}
+  state = {
+    rotation: new Animated.Value(0)
+  }
+  componentDidMount () {
+    this.interval = setInterval(() => {
+      Animated.sequence([
+        Animated.timing(this.state.rotation, {toValue: -1, duration: 150}),
+        Animated.timing(this.state.rotation, {toValue: 1, duration: 150}),
+        Animated.timing(this.state.rotation, {toValueL 0, duration: 250})
+      ]).start()
+    }, 1000)
+  }
+  componentWillUnmount () {
+    window.clearInterval(this.interval)
+  }
+  getTransform() {
+    return {
+      transform: [
+        {
+          rotate: this.state.rotation.interpolate({
+            inputRange: [-1, 1],
+            outputRange: ['-20deg', '20deg']
+          })
+        }
+      ]
+    }
+  }
   render () {
     return (
-      <View>
-        <Image
-          style={{}}
-          source={}
+      <View style={styles.container}>
+        <Animated.Image
+          style={{styles.image, this.getTransform()}}
+          source={require('../../images/react-logo.png')} />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  image: {
+    resizeMode: 'contain',
+    height: 300
+  }
+})
