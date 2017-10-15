ca4b1c8d391186160848e19d98f91b1bed818559
shivprogrammer <shiv.unadkat@gmail.com>
2017-10-14 23:24:19 -0700

    adding Facebook login to Splash

--- app/components/Splash/Splash.js
+++ app/components/Splash/Splash.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react'
 import { View, StyleSheet, Text } from 'react-native'
+import { LoginButton } from 'react-native-fbsdk'
 
 Splash.propTypes = {
 
@@ -8,13 +9,22 @@ Splash.propTypes = {
 export default function Splash (props) {
   return (
     <View>
+      <View>
+        <Image source={require('../../images/react-logo.png')} />
+        <Text>Reactive</Text>
+      </View>
+
+      <View>
+        <LoginButton
+          onLoginFinished={() => ({})}/>
+      </View>
       <Text>
-        Splash
+        Don't worry, we don't post anything to Facebook.
       </Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  
+
 })
