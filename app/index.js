a25d8cf4afc9b6e6be233ed94778235d2822fa70
shivprogrammer <shiv.unadkat@gmail.com>
2017-10-16 01:57:24 -0700

    all sorts of minor updates with adding redux

--- app/index.js
+++ app/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { AppContainer } from '~/containers'
+import { createStore, applyMiddleware, combindReducers } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
 export default function Reactive (props) {
   return (
--- app/redux/modules/authentication.js
+++ app/redux/modules/authentication.js
@@ -3,3 +3,10 @@ const initialState = {
   isAuthenticating: false,
   authedId: '',
 }
+
+export default function authentication (state = initialState, action) {
+  switch (action.type) {
+    default :
+      return state
+  }
+}
