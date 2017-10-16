64f6e434cf9fb5d3abd128fb94b8f5d7252749b1
shivprogrammer <shiv.unadkat@gmail.com>
2017-10-16 02:08:27 -0700

    successfully adding in authentication confirmations with redux

--- app/index.js
+++ app/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { AppContainer } from '~/containers'
-import { createStore, applyMiddleware, combindReducers } from 'redux'
+import { createStore, applyMiddleware, combinedReducers } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import * as reducers from './redux'
+
+const store = createStore(
+  combinedReducers(reducers),
+  applyMiddleware(thunk)
+)
 
 export default function Reactive (props) {
   return (
-    <AppContainer />
+    <Provider store={store}>
+      <AppContainer />
+    </Provider>
   )
 }
