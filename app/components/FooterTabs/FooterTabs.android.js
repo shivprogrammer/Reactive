1ddab7254d998f128ef4dd54b7442c2e8a34c479
shivprogrammer <shiv.unadkat@gmail.com>
2017-10-16 02:35:15 -0700

    progression on FooterTabs.android.js

--- app/components/FooterTabs/FooterTabs.android.js
+++ app/components/FooterTabs/FooterTabs.android.js
@@ -1,16 +1,28 @@
 import React, { PropTypes } from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, DrawerLayoutAndroid } from 'react-native'
+import { HomeContainer, LeaderboardContainer } from '~/containers'
+import Drawer from './Drawer'
 
 FooterTabs.propTypes = {
-
+  activeFooterTab: PropTypes.string.isRequired,
+  navigator: PropTypes.object.isRequired,
+  onTabSelect: PropTypes.func.isRequired
 }
 
 export default function FooterTabs (props) {
   return (
-    <View>
-      <Text>
-        FooterTabs
-      </Text>
-    </View>
+    <DrawerLayoutAndroid
+      drawerWidth={290}
+      renderNavigationView={() => (
+        <Drawer />
+      )}>
+        {props.activeFooterTab === 'home'
+          ? <HomeContainer navigator={props.navigator}/>
+          : <LeaderboardContainer navigator={props.navigator}/>}
+    </DrawerLayoutAndroid>
   )
 }
+
+const styles = StyleSheet.create({
+
+})
