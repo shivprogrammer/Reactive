8f4b9c68a14dba94ddfeb8bbc759fdac965e8dfc
shivprogrammer <shiv.unadkat@gmail.com>
2017-10-17 16:05:59 -0700

    added leaderboard container

---
+++ app/containers/LeaderboardContainer/LeaderboardContainer.js
@@ -0,0 +1,15 @@
+import React, { PropTypes, Component } from 'react'
+import { Leaderboard } from '~/components'
+
+export default class LeaderboardContainer extends Component {
+  static propTypes = {
+    openDrawer: PropTypes.func,
+    navigator: PropTypes.object.isRequired,
+  }
+  state = {}
+  render() {
+    return (
+      <Leaderboard />
+    )
+  }
+}
