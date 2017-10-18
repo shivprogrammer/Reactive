43d5f215964496dd9da623c2e7b366c499c4eab9
shivprogrammer <shiv.unadkat@gmail.com>
2017-10-18 02:43:06 -0700

    significant updates to authentication.js

--- app/redux/modules/authentication.js
+++ app/redux/modules/authentication.js
@@ -1,11 +1,50 @@
+const AUTHENTICATING = 'AUTHENTICATING'
+const NOT_AUTHED = 'NOT_AUTHED'
+const IS_AUTHED = 'IS_AUTHED'
+
+function authenticating () {
+  return {
+    type: AUTHENTICATING,
+  }
+}
+
+function notAuthed () {
+  return {
+    type: NOT_AUTHED
+  }
+}
+
+function isAuthed (uid) {
+  return {
+    type: IS_AUTHED,
+    uid
+  }
+}
+
 const initialState = {
   isAuthed: false,
   isAuthenticating: false,
   authedId: '',
 }
-
 export default function authentication (state = initialState, action) {
   switch (action.type) {
+    case AUTHENTICATING :
+      return {
+        ... state,
+        isAuthenticating: true
+      }
+    case NOT_AUTHED :
+      return {
+        isAuthenticating: false,
+        isAuthed: false,
+        authedId: ''
+      }
+    case IS_AUTHED :
+      return {
+        isAuthed: true,
+        isAuthenticating: false,
+        authedId: action.uid
+      }
     default :
       return state
   }
